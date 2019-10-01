38bd5a8c33e943ba63ae412789ddce9682350234
KeonYoungLeee <ygy5036@gmail.com>
2019-10-01 22:41:25 +0900

    modify : cookie-parser, express-session. flash

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 // logger 추가
 const logger = require('morgan');
+// cookeParse 추가
+const cookiParser = require('cookie-parser');
+// session 추가
+const session = require('express-session');
+// flash 추가
+const flash = require('connect-flash');
 const app = express();
 
 
 // logger적용시키기
 // logger는 응답시간을 요청해준다
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false }));
+
+// secret code는 비밀키 (일종의 비밀번호) , 세션의 시크릿이랑 같아야한다
+app.use(cookiParser('secret code'));
+app.use(session({
+  // 요청이 들어오면 무조건 저장 ( 낭비좀 한다. resave, saveUninitialized )
+  resave : false,
+  saveUninitialized : false,
+  secret : 'secret code',
+  cookie : {
+    httpOnly : true,
+    secure : false,
+  }
+}));
+app.use(flash());
+
 
 app.use((req, res, next) => {
   console.log("첫번째 미들웨어");
