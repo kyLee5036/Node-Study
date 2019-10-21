dd3c800138363e7be1eb18c35814d25859ed253c
KeonYoungLeee <37015435+KeonYoungLeee@users.noreply.github.com>
2019-10-21 17:05:24 +0900

    modify : API호출 (오류인경우 200, 201, 500, 404등등 )

--- learn-sequelize/public/sequelize.js
+++ learn-sequelize/public/sequelize.js
@@ -1,5 +1,5 @@
 // 사용자 이름 눌렀을 때 댓글 로딩
-document.querySelectorAll('#user-list tr').forEach(function (el) {
+document.querySelectorAll('#user-list tr').forEach(function (el) { 
     el.addEventListener('click', function () {
       var id = el.querySelector('td').textContent;
       getComment(id);
